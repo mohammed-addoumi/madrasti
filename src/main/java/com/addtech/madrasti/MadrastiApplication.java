fddf2769b8fc21e4806ffcbc71578addc4bfbcde
Mohammed Addoumi <mohammedaddoumi@hotmail.Fr>
2021-12-27 20:10:14 +0100

    creating the structure of the project

---
+++ src/main/java/com/addtech/madrasti/MadrastiApplication.java
@@ -0,0 +1,13 @@
+package com.addtech.madrasti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrastiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrastiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addtech/madrasti/MadrastiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addtech.madrasti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadrastiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
